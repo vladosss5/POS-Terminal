ae8fef936018c68f43cb8feec72817493c02a2f0
kva <kva@sncard.ru>
2026-02-18 12:39:02 +0700

    Правки по CR

--- Terminal/ViewModels/MainViewModel.cs
+++ Terminal/ViewModels/MainViewModel.cs
@@ -3,10 +3,19 @@ using Terminal.ViewModels.Pages;
 
 namespace Terminal.ViewModels;
 
+/// <summary>
+/// Логика работы главного окна.
+/// </summary>
 public partial class MainViewModel : ViewModelBase
 {
+    /// <summary>
+    /// Отображаемая страница на еткущий момент.
+    /// </summary>
     [ObservableProperty] private PageViewModelBase _currentPage;
 
+    /// <summary>
+    /// Конструктор.
+    /// </summary>
     public MainViewModel(MainMenuPageViewModel mainMenuPageViewModel)
     {
         _currentPage = mainMenuPageViewModel;
--- Terminal/ViewModels/Pages/MainMenuPageViewModel.cs
+++ Terminal/ViewModels/Pages/MainMenuPageViewModel.cs
@@ -3,8 +3,14 @@ using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace Terminal.ViewModels.Pages;
 
+/// <summary>
+/// Логика работы страницы главного меню.
+/// </summary>
 public partial class MainMenuPageViewModel : PageViewModelBase
 {
+    /// <summary>
+    /// Конструктор.
+    /// </summary>
     public MainMenuPageViewModel()
     {
         Title = "Главная";

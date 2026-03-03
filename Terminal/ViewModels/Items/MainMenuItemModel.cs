7e2267e3abc0b28d9abaa423efb7113161b92a9d
kva <kva@sncard.ru>
2026-03-03 10:24:55 +0700

    Отрисовка главного меню переделана на ItemsControl

---
+++ Terminal/ViewModels/Items/MainMenuItemModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows.Input;
+
+namespace Terminal.ViewModels.Items;
+
+public class MainMenuItemModel
+{
+    public string Title { get; set; }
+    public ICommand? Command { get; set; }
+    public object Parameter { get; set; }
+}
--- Terminal/ViewModels/Pages/RefuelingByCardPageViewModel.cs
+++ Terminal/ViewModels/Pages/RefuelingByCardPageViewModel.cs
@@ -12,7 +12,7 @@ using Terminal.Core.DbEntities;
 using Terminal.Core.Enums;
 using Terminal.Core.Models;
 using Terminal.Data.Context;
-using Terminal.ViewModels.Steps;
+using StepViewModelBase = Terminal.ViewModels.Items.StepViewModelBase;
 
 namespace Terminal.ViewModels.Pages;
 

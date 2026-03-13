03eee9e9096371858fab6f93763efd2d61626aae
kva <kva@sncard.ru>
2026-03-13 14:50:37 +0700

    Добавлено конфигурирование приложения через .json Добавлена первичное конфигурирование типов оплаты

---
+++ Terminal.Application/Interfaces/Services/IConfigurationService.cs
@@ -0,0 +1,23 @@
+﻿using Terminal.Core.Models;
+
+namespace Terminal.Application.Interfaces.Services;
+
+/// <summary>
+/// Сервис для работы с конфигурацией приложения.
+/// </summary>
+public interface IConfigurationService
+{
+    /// <summary>
+    /// Получить секцию конфигурации по модели.
+    /// </summary>
+    /// <param name="sectionName">Наименование секции.</param>
+    /// <typeparam name="T">Модель значения секции.</typeparam>
+    /// <returns>Секция конфигурации.</returns>
+    public T? GetSection<T>(string sectionName) where T : class;
+    
+    /// <summary>
+    /// Получить включенные типы оплат.
+    /// </summary>
+    /// <returns>Множество моделей настроек типа оплаты.</returns>
+    public IEnumerable<PaymentTypeSetting>? GetPaymentTypeSettings();
+}

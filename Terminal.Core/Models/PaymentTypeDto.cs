88c96c7966c66c410832bbee0cd0b28bdbb745f8
vladosss5 <kiporovv1@gmail.com>
2026-03-18 17:50:40 +0700

    Изменены модели настроек типов оплаты

--- Terminal.Core/Models/PaymentTypeDto.cs
+++ Terminal.Core/Models/PaymentTypeDto.cs
@@ -5,13 +5,13 @@ namespace Terminal.Core.Models;
 /// <summary>
 /// Модель конфигурации типа оплаты.
 /// </summary>
-public class PaymentTypeSetting
+public class PaymentTypeDto
 {
     /// <summary>
     /// Отображаемое наименование.
     /// </summary>
-    public string DisplayedName { get; set; }
-    
+    public string DisplayedName { get; set; } = null!;
+
     /// <summary>
     /// Базовый тип.
     /// </summary>

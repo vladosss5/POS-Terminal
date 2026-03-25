cd194b6d5df0fc431a0722ee02752722d12bd2d5
kva <kva@sncard.ru>
2026-03-25 15:23:41 +0700

    Добавлено настраивание времени ожидания ввода пароля

---
+++ Terminal.Core/Models/TimeoutOption.cs
@@ -0,0 +1,17 @@
+﻿namespace Terminal.Core.Models;
+
+/// <summary>
+/// Варианты времени ожидания.
+/// </summary>
+public record struct TimeoutOption
+{
+    /// <summary>
+    /// Кол-во секунд.
+    /// </summary>
+    public int Seconds { get; set; }
+    
+    /// <summary>
+    /// Св-во для показа секунд.
+    /// </summary>
+    public string DisplayName => $"{Seconds} сек.";
+}
---
+++ Terminal/ViewModels/Pages/SettingsShiftOpeningPageViewModel.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Terminal.Application.Interfaces.Services;
+using Terminal.Core.Models;
+
+namespace Terminal.ViewModels.Pages;
+
+/// <summary>
+/// Логика работы страницы с настройками открытия смены.
+/// </summary>
+public class SettingsShiftOpeningPageViewModel : PageViewModelBase
+{
+    /// <inheritdoc cref="IConfigurationService" />
+    private readonly IConfigurationService _configurationService;
+
+    /// <summary>
+    /// Выбранный вариант времени ожидания ввода пароля.
+    /// </summary>
+    public TimeoutOption SecondsAuthenticationCanceled 
+    {
+        get;
+        set
+        {
+            if (SetProperty(ref field, value))
+                _ = SaveSecondsAuthenticationCanceledAsync(value.Seconds);
+        } 
+    }
+
+    /// <summary>
+    /// Варианты времени ожидания ввода пароля.
+    /// </summary>
+    public HashSet<TimeoutOption> TimeoutValues { get; } = 
+    [
+        new() { Seconds = 10 },
+        new() { Seconds = 15 },
+        new() { Seconds = 30 },
+        new() { Seconds = 60 }
+    ];
+    
+    /// <summary>
+    /// Конструктор.
+    /// </summary>
+    public SettingsShiftOpeningPageViewModel(
+        ILogger<PageViewModelBase> logger, 
+        IConfigurationService configurationService) 
+        : base(logger)
+    {
+        Title = "Открытие смены";
+        
+        _configurationService = configurationService;
+        
+        _ = InitializeDataAsync();
+    }
+
+    /// <summary>
+    /// Инициализировать данные.
+    /// </summary>
+    private async Task InitializeDataAsync()
+    {
+        var timeOutValue = await _configurationService
+            .GetValueAsync<int>("SecondsAuthenticationCanceled");
+
+        SecondsAuthenticationCanceled = new TimeoutOption { Seconds = timeOutValue };
+        TimeoutValues.Add(SecondsAuthenticationCanceled);
+    }
+
+    /// <summary>
+    /// Сохранить выбор времени ожидания ввода пароля.
+    /// </summary>
+    /// <param name="value">Кол-во секунд.</param>
+    private async Task SaveSecondsAuthenticationCanceledAsync(int value)
+    {
+        await _configurationService.SetValueAsync("SecondsAuthenticationCanceled", value);
+    }
+}

1320debc1ce4d76c0fbcbfcd613bd0a0ad9a2491
kva <kva@sncard.ru>
2026-03-27 10:47:06 +0700

    Добавлена верхняя панель для alps S200

---
+++ Terminal.Core/Models/DeviceInfoModel.cs
@@ -0,0 +1,29 @@
+﻿using Terminal.Core.Enums;
+
+namespace Terminal.Core.Models;
+
+/// <summary>
+/// Информация об устройстве на котором запущено приложение.
+/// </summary>
+public class DeviceInfoModel
+{
+    /// <summary>
+    /// Платформа.
+    /// </summary>
+    public DevicePlatform Platform { get; set; }
+    
+    /// <summary>
+    /// Производитель.
+    /// </summary>
+    public string? Manufacturer { get; set; }
+    
+    /// <summary>
+    /// Модель.
+    /// </summary>
+    public string? Model { get; set; }
+    
+    /// <summary>
+    /// Наименование.
+    /// </summary>
+    public string? Name { get; set; }
+}

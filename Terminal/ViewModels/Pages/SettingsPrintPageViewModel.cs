504d89dc4c1de1022fd0f29358cc6bfdc53a3a6b
kva <kva@sncard.ru>
2026-03-27 16:36:36 +0700

    Настройки печати прикручены ко View

--- Terminal/ViewModels/Pages/SettingsPrintPageViewModel.cs
+++ Terminal/ViewModels/Pages/SettingsPrintPageViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Extensions.Logging;
+using Terminal.Application.Interfaces.Services;
+using Terminal.Core.Models.Settings;
 
 namespace Terminal.ViewModels.Pages;
 
@@ -7,15 +9,30 @@ namespace Terminal.ViewModels.Pages;
 /// </summary>
 public class SettingsPrintPageViewModel : PageViewModelBase
 {
+    /// <inheritdoc cref="IConfigurationService" />
+    private readonly IConfigurationService _configurationService;
+
+    /// <summary>
+    /// Св-во для получения ссылки на экземпляр класса текущей конфигурации.
+    /// </summary>
+    public SettingsModel SettingsModel { get; }
+    
     /// <summary>
     /// Конструктор.
     /// </summary>
     public SettingsPrintPageViewModel(
-        ILogger<PageViewModelBase> logger) 
+        ILogger<PageViewModelBase> logger, 
+        IConfigurationService configurationService) 
         : base(logger)
     {
+        _configurationService = configurationService;
         Title = "Настройки печати";
+
+        SettingsModel = _configurationService.CurrentSetting;
     }
     
-    
+    /// <summary>
+    /// Вызвать сохранение конфигурации в файл.
+    /// </summary>
+    public void SaveCommand() => _configurationService.SaveSettingsToFile();
 }

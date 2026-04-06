8473f9fa4f204190f933a50a1d635533e3aa98d4
kva <kva@sncard.ru>
2026-04-06 15:30:53 +0700

    Добавлен контекст БД параметров

--- Terminal/Extensions/ServiceCollectionExtensions.cs
+++ Terminal/Extensions/ServiceCollectionExtensions.cs
@@ -72,8 +72,7 @@ public static class ServiceCollectionExtensions
     public static void AddDataContext(this IServiceCollection collection)
     {
         var dbPath = DataContext.GetDefaultDbPath();
-        
-        string? dir = Path.GetDirectoryName(dbPath);
+        var dir = Path.GetDirectoryName(dbPath);
         
         if (dir != null)
             Directory.CreateDirectory(dir);
@@ -88,6 +87,18 @@ public static class ServiceCollectionExtensions
                 .EnableDetailedErrors()
                 .LogTo(Console.WriteLine, LogLevel.Information);
 #endif
+        });
+
+        
+        var paramDbPath = ParamDbContext.GetDefaultDbPath();
+        var paramDbDir = Path.GetDirectoryName(paramDbPath);
+        
+        if (paramDbDir != null)
+            Directory.CreateDirectory(paramDbDir);
+
+        collection.AddDbContextFactory<ParamDbContext>(options =>
+        {
+            options.UseSqlite($"Data Source={paramDbPath}");
         });
     }
 

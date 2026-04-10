40fd496db9a66c853c3f6bdaf225344e4ac59eee
kva <kva@sncard.ru>
2026-04-10 13:05:04 +0700

    Готово диалоговое окно с чеком

--- Terminal/Services/DialogPrintService.cs
+++ Terminal/Services/DialogPrintService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Avalonia.Controls;
@@ -133,9 +134,66 @@ public class DialogPrintService
         AppendKeyValueLine("Начало:", reportData.Shift.ShiftDate != null ? reportData.Shift.ShiftDate!.Value.ToString(culture) : "");
         AppendKeyValueLine("Конец:", DateTime.Now.ToString(culture));
 
-        foreach (var saleData in reportData.SalesList)
+        var issuersCount = reportData.SalesList
+            .Select(x => x.ICI)
+            .GroupBy(x => x!.Value)
+            .Count();
+
+        AppendLineWidth();
+        PrintOperationsOnIssuer(
+            $"Эмитент {reportData.IssuerNumber}",
+            reportData.SalesList.Where(x => x.ICI!.Value == Convert.ToInt32(reportData.IssuerNumber)),
+            issuersCount > 1);
+
+        if (issuersCount > 1)
+            PrintOperationsOnIssuer(
+                $"Другие эмитенты",
+                reportData.SalesList.Where(x => x.ICI!.Value != Convert.ToInt32(reportData.IssuerNumber)),
+                true);
+        
+        
+        var totalData = new
         {
-            AppendLineWidth(saleData.N ?? "");
+            TotalBaseCost = reportData.SalesList.Sum(x => x.SBC ?? 0),
+            TotalSC = reportData.SalesList.Sum(x => x.SC ?? 0),
+            TotalSBCR = reportData.SalesList.Sum(x => x.SBCR ?? 0),
+            TotalSCR = reportData.SalesList.Sum(x => x.SCR ?? 0)
+        };
+        
+        AppendLineWidth("Итого в чеке");
+        AppendTextInCenter("Итого продаж");
+        AppendKeyValueLine("Сумма баз.", totalData.TotalBaseCost.ToString(culture));
+        AppendKeyValueLine("Сумма скид.", totalData.TotalSC.ToString(culture));
+        
+        AppendTextInCenter("Итого возвратов");
+        AppendKeyValueLine("Сумма баз.", totalData.TotalSBCR.ToString(culture));
+        AppendKeyValueLine("Сумма скид.", totalData.TotalSCR.ToString(culture));
+        
+        AppendTextInCenter("Всего продаж");
+        AppendKeyValueLine("Сумма баз.", (totalData.TotalBaseCost - totalData.TotalSBCR).ToString(culture));
+        AppendKeyValueLine("Сумма скид.", (totalData.TotalSC - totalData.TotalSCR).ToString(culture));
+        
+        AppendTextInCenter();
+        _stringBuilder.AppendLine($"Оператор: {reportData.OperatorName}");
+        AppendLineWidth();
+        AppendTextInCenter();
+        AppendTextInCenter();
+        AppendLineWidth();
+        AppendTextInCenter("Подпись");
+        
+        return _stringBuilder.ToString();
+    }
+
+    private void PrintOperationsOnIssuer(string issuerName, IEnumerable<SalesReportResult> operations, bool isPrintTotal)
+    {
+        var culture = new CultureInfo("ru-RU");
+        
+        AppendTextInCenter(issuerName);
+        
+        foreach (var saleData in operations)
+        {
+            var resourceName = !string.IsNullOrEmpty(saleData.N) ? saleData.N! : "undefined";
+            AppendLineWidth(resourceName);
             
             AppendTextInCenter("Продажи");
             AppendKeyValueLine("Ко-во", saleData.A != null ? saleData.A.Value.ToString(culture) : "0");
@@ -146,17 +204,39 @@ public class DialogPrintService
             AppendKeyValueLine("Ко-во", saleData.AR != null ? saleData.AR.Value.ToString(culture) : "0");
             AppendKeyValueLine("Сумма баз.", saleData.SBCR != null ? saleData.SBCR.Value.ToString(culture) : "0");
             AppendKeyValueLine("Сумма скид.", saleData.SCR != null ? saleData.SCR.Value.ToString(culture) : "0");
+            
+            AppendTextInCenter($"Итого по {resourceName}");
+            AppendKeyValueLine("Ко-во", ((saleData.A ?? 0) - (saleData.AR ?? 0)).ToString(culture));
+            AppendKeyValueLine("Сумма баз.", ((saleData.SBC ?? 0) - (saleData.SBCR ?? 0)).ToString(culture));
+            AppendKeyValueLine("Сумма скид.", ((saleData.SC ?? 0) - (saleData.SCR ?? 0)).ToString(culture));
         }
 
-        AppendTextInCenter();
-        _stringBuilder.AppendLine($"Оператор: {reportData.OperatorName}");
         AppendLineWidth();
-        AppendTextInCenter();
-        AppendTextInCenter();
-        AppendLineWidth();
-        AppendTextInCenter("Подпись");
+
+        if (!isPrintTotal)
+            return;
         
-        return _stringBuilder.ToString();
+        var totalData = new
+        {
+            TotalSBC = operations.Sum(x => x.SBC ?? 0),
+            TotalSC = operations.Sum(x => x.SC ?? 0),
+            TotalSBCR = operations.Sum(x => x.SBCR ?? 0),
+            TotalSCR = operations.Sum(x => x.SCR ?? 0)
+        };
+        
+        AppendTextInCenter("Итого продаж");
+        AppendKeyValueLine("Сумма баз.", totalData.TotalSBC.ToString(culture));
+        AppendKeyValueLine("Сумма скид.", totalData.TotalSC.ToString(culture));
+        
+        AppendTextInCenter("Итого возвратов");
+        AppendKeyValueLine("Сумма баз.", totalData.TotalSBCR.ToString(culture));
+        AppendKeyValueLine("Сумма скид.", totalData.TotalSCR.ToString(culture));
+        
+        AppendTextInCenter("Всего продаж");
+        AppendKeyValueLine("Сумма баз.", (totalData.TotalSBC - totalData.TotalSBCR).ToString(culture));
+        AppendKeyValueLine("Сумма скид.", (totalData.TotalSC - totalData.TotalSCR).ToString(culture));
+        
+        AppendLineWidth();
     }
 
     /// <summary>

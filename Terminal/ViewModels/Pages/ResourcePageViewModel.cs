b79af0de412bdc57016b36c9e5a27c8fbbe7bee1
kva <kva@sncard.ru>
2026-04-21 17:59:59 +0700

    Добавлена пустая страница управления ресурсов

---
+++ Terminal/ViewModels/Pages/ResourcePageViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Terminal.Core.DbEntities;
+using Terminal.Data.Context;
+
+namespace Terminal.ViewModels.Pages;
+
+public class ResourcePageViewModel : PageViewModelBase
+{
+    /// Фабрика создающая <inheritdoc cref="DataContext"/>
+    private readonly IDbContextFactory<DataContext> _dbFactory;
+    
+    public ObservableCollection<ResourceCode> Resources
+    {
+        get; 
+        set => SetProperty(ref field, value);
+    }
+        
+    public ResourcePageViewModel(
+        ILogger<PageViewModelBase> logger, 
+        IDbContextFactory<DataContext> dbFactory) 
+        : base(logger)
+    {
+        _dbFactory = dbFactory;
+
+        _ = LoadData();
+    }
+
+    private async Task LoadData()
+    {
+        await using var db = await _dbFactory.CreateDbContextAsync();
+
+        var resources = await db.ResourceCodes.ToListAsync();
+        Resources = new ObservableCollection<ResourceCode>(resources);
+    }
+}
---
+++ Terminal/Views/Pages/ResourcePageView.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Terminal.Views.Pages;
+
+public partial class ResourcePageView : UserControl
+{
+    public ResourcePageView()
+    {
+        InitializeComponent();
+    }
+}

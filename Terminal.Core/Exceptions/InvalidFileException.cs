3da97f563f32e496706bf88afaa4661e28ecf32a
kva <kva@sncard.ru>
2026-06-22 16:57:50 +0700

    Добавлены модели ошибок некорректности файлов и ненайденного рессурса

---
+++ Terminal.Core/Exceptions/InvalidFileException.cs
@@ -0,0 +1,7 @@
+﻿namespace Terminal.Core.Exceptions;
+
+/// <summary>
+/// Некорректный файл.
+/// </summary>
+/// <param name="message">Сообщение об ошибке.</param>
+public class InvalidFileException(string message) : Exception(message);
---
+++ Terminal.Core/Exceptions/NotFoundException.cs
@@ -0,0 +1,7 @@
+﻿namespace Terminal.Core.Exceptions;
+
+/// <summary>
+/// Объект не найден.
+/// </summary>
+/// <param name="message">Сообщение.</param>
+public class NotFoundException(string message) : Exception(message);

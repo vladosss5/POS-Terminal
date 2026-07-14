1581b8f20714b8d042d1f91c84c7809cade182eb
kva <kva@sncard.ru>
2026-07-14 11:28:09 +0700

    Добавлен репозиторий для рессурсов

---
+++ Terminal.Core/IRepositories/IResourceCodeRepository.cs
@@ -0,0 +1,8 @@
+﻿using Terminal.Core.Entities.DbEntities.MainDb;
+
+namespace Terminal.Core.IRepositories;
+
+public interface IResourceCodeRepository
+{
+    public Task<List<ResourceCode>> GetShowedResourceCodesAsync();
+}
---
+++ Terminal.Persistence/Repositories/ResourceCodeRepository.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Terminal.Core.Entities.DbEntities.MainDb;
+using Terminal.Core.IRepositories;
+using Terminal.Persistence.MainDB;
+
+namespace Terminal.Persistence.Repositories;
+
+public class ResourceCodeRepository : IResourceCodeRepository
+{
+    private readonly DataContext _dataContext;
+    
+    public ResourceCodeRepository(IDbContextFactory<DataContext> mainDbFactory)
+    {
+        _dataContext = mainDbFactory.CreateDbContext();
+    }
+    
+    public async Task<List<ResourceCode>> GetShowedResourceCodesAsync()
+    {
+        var products = await _dataContext.ResourceCodes
+            .Where(x => x.IsShow == 1)
+            .OrderBy(p => p.ResourceName)
+            .AsNoTracking()
+            .ToListAsync();
+
+        return products;
+    }
+}

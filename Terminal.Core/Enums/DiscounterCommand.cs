65b2a3d7aeedf8c24a228bf6cfc0f3e6137a25a9
kva <kva@sncard.ru>
2026-07-24 11:53:55 +0700

    Дополнен набор типов команд для работы с библиотекой скидок

--- Terminal.Core/Enums/DiscounterCommand.cs
+++ Terminal.Core/Enums/DiscounterCommand.cs
@@ -7,11 +7,75 @@ namespace Terminal.Core.Enums;
 /// </summary>
 public enum DiscounterCommand
 {
-    /// <summary>Рассчитать скидку</summary>
+    /// <summary>
+    /// Рассчитать скидку.
+    /// </summary>
     [XmlEnum("0")]
-    CalculateDiscount = 0,
+    CalculateDiscount,
     
-    /// <summary>Получить информацию по карте</summary>
+    /// <summary>
+    /// Получить сообщения по карте.
+    /// </summary>
+    [XmlEnum("1")]
+    Message,
+    
+    /// <summary>
+    /// Получить информацию по карте.
+    /// </summary>
     [XmlEnum("2")]
-    GetCardInfo = 2
+    GetCardInfo,
+    
+    /// <summary>
+    /// Зарегистрировать продажу в онлайне.
+    /// </summary>
+    [XmlEnum("3")]
+    OnlineConfirm,
+    
+    /// <summary>
+    /// Отменить регистрацию продажи в онлайне.
+    /// </summary>
+    [XmlEnum("4")]
+    OnlineReset,
+    
+    /// <summary>
+    /// Подтвердить продажу.
+    /// </summary>
+    [XmlEnum("5")]
+    OnlineComplete,
+    
+    /// <summary>
+    /// Проверить ограничения карты.
+    /// </summary>
+    [XmlEnum("6")]
+    Limitation,
+    
+    /// <summary>
+    /// Начислить сдачу в качестве бонусов.
+    /// </summary>
+    [XmlEnum("7")]
+    ChangeBonus,
+    
+    /// <summary>
+    /// ХЗ.
+    /// </summary>
+    [XmlEnum("8")]
+    PostProcessing,
+    
+    /// <summary>
+    /// Зарегистрировать карту.
+    /// </summary>
+    [XmlEnum("9")]
+    RegistrationCard,
+    
+    /// <summary>
+    /// Проверить продажу в онлайне.
+    /// </summary>
+    [XmlEnum("10")]
+    OnlineCheck,
+    
+    /// <summary>
+    /// Обновить переоценки.
+    /// </summary>
+    [XmlEnum("11")]
+    UpdateMarkdown
 }

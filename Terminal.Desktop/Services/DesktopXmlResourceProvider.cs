9e8f234cd0a49943b7699efa512ab7f7c715d31a
kva <kva@sncard.ru>
2026-08-06 12:26:05 +0700

    Реализовано чтение xml фалов проекта в Desktop реализации

--- Terminal.Desktop/Services/DesktopXmlResourceProvider.cs
+++ Terminal.Desktop/Services/DesktopXmlResourceProvider.cs
@@ -1,16 +1,44 @@
-﻿using System.Threading;
+﻿using System;
+using System.IO;
+using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 using Terminal.Core.Interfaces;
 
 namespace Terminal.Desktop.Services;
 
 public class DesktopXmlResourceProvider : IXmlResourceProvider
 {
-    public async Task<string> LoadXmlContentAsync(string fileName, CancellationToken cancellationToken = default)
+    /// <summary>
+    /// Логгер.
+    /// </summary>
+    private readonly ILogger<DesktopXmlResourceProvider> _logger;
+
+    /// <summary>
+    /// Конструктор.
+    /// </summary>
+    public DesktopXmlResourceProvider(ILogger<DesktopXmlResourceProvider> logger)
     {
-        throw new System.NotImplementedException();
+        _logger = logger;
     }
 
+    /// <inheritdoc/>
+    public async Task<string> LoadXmlContentAsync(string fileName, CancellationToken cancellationToken = default)
+    {
+        var path = Path.Combine(Directory.GetCurrentDirectory(), "Xmls", fileName);
+
+        if (!File.Exists(path))
+        {
+            _logger.LogError($"Файл {fileName} не найден.");
+            throw new Exception($"Файл {fileName} не найден.");
+        }
+
+        var resultText = await File.ReadAllTextAsync(path, cancellationToken);
+
+        return resultText;
+    }
+
+    /// <inheritdoc/>
     public async Task<bool> XmlFileExistsAsync(string fileName, CancellationToken cancellationToken = default)
     {
         throw new System.NotImplementedException();

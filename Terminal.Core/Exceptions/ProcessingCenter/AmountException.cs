564a69dbc6069c016ce54dc46d3a533835ffb396
kva <kva@sncard.ru>
2026-08-17 12:14:18 +0700

    Добавлены ошибки RequiredParameter, RequiredPinCode, Amount и их обработка в бизнесс-логике.

---
+++ Terminal.Core/Exceptions/ProcessingCenter/AmountException.cs
@@ -0,0 +1,9 @@
+﻿namespace Terminal.Core.Exceptions.ProcessingCenter;
+
+/// <summary>
+/// Ошибка ПЦ: Запрошенное кол-во больше доступного.
+/// </summary>
+public class AmountException : Exception
+{
+    
+}
---
+++ Terminal.Core/Exceptions/ProcessingCenter/RequiredParameterException.cs
@@ -0,0 +1,9 @@
+﻿namespace Terminal.Core.Exceptions.ProcessingCenter;
+
+/// <summary>
+/// Ошибка ПЦ: В запросе требуются дополнительные параметры. 
+/// </summary>
+public class RequiredParameterException : Exception
+{
+    
+}
